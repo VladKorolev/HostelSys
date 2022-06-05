36a537d3f7a8bb3a43fbb80a024c23615ccae25b
Vlad <vlad.korolev.90.90@mail.ru>
2022-06-05 17:36:51 +0700

    TestAdd
    ЮнитТесты

---
+++ HotelSystemTests/ViewModel/MainWindowViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotelSystem.ViewModel;
+using HotelSystem.Model;
+using HotelSystem.HotelDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.ViewModel.Tests
+{
+    [TestClass()]
+    public class MainWindowViewModelTests
+    {
+        [TestMethod()]
+        public void MainWindowViewModelTest()
+        {
+            Client client = new Client();         
+            client.Passport = "1111-111111";
+            int expectedsize = 11;                            
+
+            Assert.AreEqual(expectedsize, client.Passport.Length);
+        }
+    }
+}
---
+++ HotelSystemTests1/ViewModel/MainWindowViewModelTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotelSystem.ViewModel;
+using HotelSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.ViewModel.Tests
+{
+    [TestClass()]
+    public class MainWindowViewModelTests
+    {
+        [TestMethod()]
+        public void MainWindowViewModelTest()
+        {
+            Room room = new Room();
+            room.Number = "5";
+            
+            
+            int expected = 0;
+            expected.GetType();
+
+            Assert.AreEqual(expected.GetType(), Convert.ToInt32(room.Number).GetType());
+        }
+    }
+}
